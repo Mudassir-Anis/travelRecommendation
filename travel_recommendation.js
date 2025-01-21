4757c97d1b8456fe874fc63a2e9e261c4fb8b5eb
Mudassir Anis <muhammadmudassir.786anis@gmail.com>
2025-01-21 10:23:37 -0500

    message

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,10 +1,12 @@
 const searchEl = document.getElementById('inp-field');
 const searchBtn = document.getElementById('search-btn');
+const heroRight = document.getElementById('hero-section-right');
 
 const showRecommendation = (data) => {
     const str = searchEl.value.toLowerCase();
     let place = findKey(str,data);
     
+    updatePage(place);
     console.log(data);
     console.log(str);
     console.log(place);
@@ -33,6 +35,20 @@ function findKey(str,data) {
 
     return data[key];
 }
-
+function updatePage(data) {
+    heroRight.innerHTML = '';
+    let content;
+    data.forEach((elem) => {
+        content = `<div id="recommendation-card" class="recommendation-card">
+        <img class="recommendation-img" src="${elem.imageUrl}"/>
+        <div id="recommendation-details" class="recommendation-details">
+            <h2 id="recommendation-heading" class="recommendation-heading">${elem.name}</h2>
+            <p id="recommendation-description" class="recommendation-description">${elem.description}</p>
+            <div id="visit" class="visit">Visit</div>
+        </div>
+   </div>`
+        heroRight.insertAdjacentHTML("beforeend",content);
+    })
+}
 
 
