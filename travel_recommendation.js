d061727d01f6333216f2eea613f4ce9c8b0ef8e6
Mudassir Anis <muhammadmudassir.786anis@gmail.com>
2025-01-21 08:19:12 -0500

    message

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,21 +1,38 @@
 const searchEl = document.getElementById('inp-field');
 const searchBtn = document.getElementById('search-btn');
 
+const showRecommendation = (data) => {
+    const str = searchEl.value.toLowerCase();
+    let place = findKey(str,data);
+    
+    console.log(data);
+    console.log(str);
+    console.log(place);
+}
+
 const getData = async () => {
     const rawData = await fetch("travel_recommendation_api.json");
     const data = await rawData.json();
     showRecommendation(data);
+   
 };
-const showRecommendation = (data) => {
-    const str = searchEl.ariaValueMax.toLocaleLowerCase();
-    // for(key in data) {
-    //     const place = data[key].find((elem) => elem.name === inpStr || elem.name === inpStr + 's');
 
-    // }
-    console.log(str);
-}
 searchBtn.addEventListener('click', getData);
 
+function findKey(str,data) {
+    let key;
+    if(str in data) {
+        key = str;
+    }
+    else if ((str + 's') in data) {
+        key = str + 's'; 
+    }
+    else if((str + 'es') in data) {
+        key = str + 'es';
+    }
+
+    return data[key];
+}
 
 
 
